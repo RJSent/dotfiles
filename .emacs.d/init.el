92242302cab01b15cac53c36b04f49e45791feb9
RJSent <richard.j.sent@tutamail.com>
2020-11-05 20:46:22 -0500

    Added straight.el as a replacement for package.el.
    Supports version locking and packages are now stored
    as git repos. Easier to version lock with straight-freeze-versions.
    Restore with straight-thaw-versions.
    
    Also redid the comments and organization to better match the
    elisp conventions. No need for a custom minor mode solely to avoid
    flycheck yelling at me! :)
    
    Some small miscellaneous improvements, such as toggle-frame-fullscreen
    being replaced. Now Emacs will simply go fullscreen whenever
    init.el is evaluated, not toggle.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,56 +1,61 @@
-; Evaluate a buffer with M-x eval-buffer. Using this in init.el buffer will make Emacs apply any changes without restarting
-;; Learn about the major mode and any minor modes that are active for the current buffer with C-h m, describe-mode
-;; Learn about a function with C-h f, describe-function
-;; Learn about a variable with C-h v, describe-variable
-;; Learn about a key binding with C-h k, describe-key. Useful if I remember a binding but not what it does
+;;; init.el --- Initialization file for Emacs
+;;
+;;
+;;; Commentary:
+;; It's what you'd expect.
+;;
+;;; Code:
 
-;;; Packages
-;; Autoinstall use-package if we don't have it installed
-(require 'package)
-(setq package-enable-at-startup nil)
-(add-to-list 'package-archives '("MELPA"        . "https://melpa.org/packages/"))
-(add-to-list 'package-archives '("MELPA Stable" . "https://stable.melpa.org/packages/"))
-(add-to-list 'package-archive-priorities '("MELPA"        . 10))
-(add-to-list 'package-archive-priorities '("MELPA Stable" . 5))
-(add-to-list 'package-archive-priorities '("gnu"          . 0))
-(package-initialize)
 
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-(eval-when-compile
-  (require 'use-package))
+(defvar bootstrap-version)		; Install straight.el
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+(straight-use-package 'use-package)	; Install use-package
+
+
+;;; Navigation, aesthetic, and other global packages
+
 (use-package ace-window
-  :ensure t
+  :straight t
   :bind* ("M-o" . 'ace-window) ; * as ibuffer overrides M-o. Consider adjusting as M-o is used for ivy-dispatching done
   :config (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l)))
 (use-package which-key
-  :ensure t
+  :straight t
   :defer 0.2
   :diminish
   :config (which-key-mode))
 (use-package all-the-icons
-  :ensure t)
+  :straight t)
+(use-package diminish
+  :straight t)
 (use-package nord-theme ; I prefer Nord but with the hc-zenburn modeline
-  :ensure t
+  :straight t
   :config (load-theme 'nord t))
 ;; (use-package hc-zenburn-theme
-;;   :ensure t
+;;   :straight t
 ;;   :config (load-theme 'hc-zenburn t))
 (use-package uniquify
-  :ensure nil
   :config (setq uniquify-buffer-name-style 'forward))
 (use-package projectile
-  :ensure t
+  :straight t
   :diminish
   :config (projectile-mode +1)
   :bind-keymap ("C-c p" . projectile-command-map))
 (use-package all-the-icons-ibuffer
-  :ensure t
+  :straight t
   :diminish
   :after all-the-icons)
 (use-package ibuffer-vc ; Also consider ibuffer-projectile
-  :ensure t
+  :straight t
   :after all-the-icons-ibuffer
   :hook (ibuffer . (lambda () (ibuffer-vc-set-filter-groups-by-vc-root)
 		     (unless (eq ibuffer-sorting-mode 'alphabetic)
@@ -77,34 +82,10 @@
 		(vc-status 16 16 :left)
 		" "
 		vc-relative-file))))
-(use-package company
-  :ensure t
-  :diminish
-  :hook (prog-mode . company-mode)
-  :config
-  (setq company-minimum-prefix-length 1)
-  (setq company-frontends '(company-pseudo-tooltip-frontend
-			    company-echo-metadata-frontend))
-  (setq company-idle-delay 1))
-(use-package smartparens
-  :ensure t
-  :diminish
-  :hook (prog-mode . smartparens-mode) ; FIXME not working for enh-ruby-mode although that is part of prog-mode
-  :config (require 'smartparens-config))
-(use-package highlight-parentheses
-  :ensure t
-  :diminish
-  :hook (prog-mode . highlight-parentheses-mode))
-(use-package flycheck
-  :ensure t
-  :diminish
-  :config (global-flycheck-mode))
-(use-package diminish
-  :ensure t)
 (use-package doom-modeline ; Later, replace with custom following similar process to https://www.gonsie.com/blorg/modeline.html
-  :ensure t
+  :straight t
   :hook (after-init . doom-modeline-mode)
-  :custom    
+  :custom
   (doom-modeline-height 20)
   (doom-modeline-bar-width 1)
   (doom-modeline-icon t)
@@ -123,18 +104,46 @@
   (doom-modeline-irc-stylize 'identity)
   (doom-modeline-github-timer nil)
   (doom-modeline-gnus-timer nil))
+(use-package highlight-parentheses
+  :straight t
+  :diminish
+  :hook (prog-mode . highlight-parentheses-mode))
 
+
+;;; Packages for general purpose programming and editing
+
+(use-package company
+  :straight t
+  :diminish
+  :hook (prog-mode . company-mode)
+  :config
+  (setq company-minimum-prefix-length 1)
+  (setq company-frontends '(company-pseudo-tooltip-frontend
+			    company-echo-metadata-frontend))
+  (setq company-idle-delay 1))
+(use-package smartparens
+  :straight t
+  :diminish
+  :hook (prog-mode . smartparens-mode) ; FIXME not working for enh-ruby-mode although that is part of prog-mode
+  :config (require 'smartparens-config))
+(use-package flycheck
+  :straight t
+  :diminish
+  :config (global-flycheck-mode))
+
+
+;;; Packages for ivy and ivy integration
 
 (use-package ivy
   :demand
-  :ensure t
+  :straight t
   :diminish
   :config
   (setq ivy-use-virtual-buffers t
         ivy-count-format "%d/%d ")
   (ivy-mode 1))
 (use-package ivy-rich
-  :ensure t
+  :straight t
   :after ivy
   ;; :custom
   ;; (ivy-virtual-abbreviate 'full
@@ -145,101 +154,91 @@
   ;; (ivy-set-display-transformer 'ivy-switch-buffer
   ;;                              'ivy-rich-switch-buffer-transformer))
 (use-package counsel
-  :ensure t
+  :straight t
   :diminish
   :after ivy
   :config (counsel-mode))
 (use-package swiper
-  :ensure t
+  :straight t
   :after ivy
   :bind (("C-s" . swiper)
 	 ("C-r" . swiper)))
-;; (use-package all-the-icons-ivy
-;;   :ensure t
+;; (use-package all-the-icons-ivy ; Replaced with all-the-icons-ivy-rich
+;;   :straightd t
 ;;   :after (all-the-icons ivy)
 ;;   :config (all-the-icons-ivy-setup))
 (use-package all-the-icons-ivy-rich
-  :ensure t
+  :straight t
   :after (all-the-icons ivy-rich)
   :init (all-the-icons-ivy-rich-mode 1))
 (use-package counsel-projectile
-  :ensure t
+  :straight t
   :after (counsel projectile)
   :config (counsel-projectile-mode))
 
+
+;;; Ruby programming packages
+
 (use-package rbenv
-  :ensure t
+  :straight t
   :diminish
   :hook (enh-ruby-mode . global-rbenv-mode))
 (use-package enh-ruby-mode
-  :ensure t
+  :straight t
   :mode "\\.\\(?:cap\\|gemspec\\|irbrc\\|gemrc\\|rake\\|rb\\|ru\\|thor\\|Brewfile\\|Capfile\\|Gemfile\\(?:\\.[a-zA-Z0-9._-]+\\)?\\|[rR]akefile\\)\\'"
   :interpreter "ruby")
-(use-package inf-ruby ;; Latest version has --nomultline by default, but not for bundle console, only ruby
-  :ensure t
+(use-package inf-ruby ; Latest version has --nomultline by default, but not for bundle console, only ruby
+  :straight t
   :hook (enh-ruby-mode . inf-ruby-minor-mode))
 (use-package robe
-  :ensure t
+  :straight t
   :diminish
   :hook (enh-ruby-mode . robe-mode)
   :config (push 'company-robe company-backends))
 
 
-;; C-c C-c to execute source blocks
-(org-babel-do-load-languages
+;;; Some less well organized code.
+
+(org-babel-do-load-languages ; Org-babel language support
  'org-babel-load-languages
  '((emacs-lisp . t)
    (ruby . t)
    (C . t)))
 
 
-;;; Mode definitions that don't fit in with use-package yet
-(define-derived-mode mycfg-elisp-mode emacs-lisp-mode "MyConfig Elisp Mode"
-  "A mode for my Elisp configs so Flycheck doesn't yell at me... until I get around to making this pretty....")
-(add-to-list 'auto-mode-alist '("init.el" . mycfg-elisp-mode))
-(diminish 'mycfg-elisp-mode)
-
-;;; Splitting init.el across multiple files
+;; Support for custom.el
 (write-region "" "" (expand-file-name "custom.el" (file-name-directory (or load-file-name buffer-file-name))) t)
 (setq custom-file (expand-file-name "custom.el" (file-name-directory (or load-file-name buffer-file-name))))
 (load custom-file)
 
+
 ;;; Unosrted code goes here
+
 ;; Removes empty space at bottom of screen with maximized emacs.
-;; emacs.stackexchange.com/questions/34675
-(setq frame-resize-pixelwise t)
+(setq frame-resize-pixelwise t) ; emacs.stackexchange.com/questions/34675
 ;; Increases font in default buffer, which all other buffers base off of. May be overwritten by buffer specific fonts
-;; stackoverflow.com/questions/294664
-(set-face-attribute 'default nil :height 110)
+(set-face-attribute 'default nil :height 110) ; stackoverflow.com/questions/294664
 ;; Consider fixing commenting with https://stackoverflow.com/questions/26312317
 ;; Display line numbers on all files. Currently includes shell, want to disable that
 ;; https://emacs.stackexchange.com/questions/36747 likely has the solution
-;; http://ergoemacs.org/emacs/emacs_line_number_mode.html
 (global-display-line-numbers-mode)
-;; Disable menu bar? Not sure, didn't look for this online but in Emacs
-;; Yay, entirely using emacs self help system
 (menu-bar-mode -1)
-;; Disable tool bar.
 (tool-bar-mode -1)
-;; Make the initial frame maximized and fullscreen. If you exit fullscreen the frame is still maximized
-(toggle-frame-fullscreen)
-(toggle-frame-maximized)
-;; Disable scroll bar
+(set-frame-parameter nil 'fullscreen 'fullboth) ; Start emacs fullscreen, don't toggle when evaling buffer (probably best to move to init hook)
 (scroll-bar-mode -1)
-;; Disable hidden text in info files
 (setq Info-hide-note-references 1)
 ;; Inhibit startup message
 (setq inhibit-startup-message t)
 (setq ring-bell-function 'ignore)
 ;; https://www.emacswiki.org/emacs/BackupDirectory
 (setq
-   backup-by-copying t      ; don't clobber symlinks
-   backup-directory-alist
-    '(("." . "~/.saves/"))    ; don't litter my fs tree
-   delete-old-versions t
-   kept-new-versions 6
-   kept-old-versions 2
-   version-control t)       ; use versioned backups
+   backup-by-copying t			                ; don't clobber symlinks
+   backup-directory-alist				;
+    `(("." . ,(concat user-emacs-directory "backups")))	; don't litter my fs tree
+   delete-old-versions t				;
+   kept-new-versions 6					;
+   kept-old-versions 2					;
+   version-control t)			                ; use versioned backups
 
 ;; SORT ME
 (delete-selection-mode 1)
