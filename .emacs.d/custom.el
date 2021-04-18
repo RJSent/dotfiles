e8d7c0561f50a4eb5efed5b785c3cb05311f05e1
RJSent <richard.j.sent@tutamail.com>
2021-04-18 11:23:03 -0400

    added safe file local variable

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -6,7 +6,8 @@
  '(custom-safe-themes
    '("d74c5485d42ca4b7f3092e50db687600d0e16006d8fa335c69cf4f379dbd0eee" "d19f00fe59f122656f096abbc97f5ba70d489ff731d9fa9437bac2622aaa8b89" "a37d20710ab581792b7c9f8a075fcbb775d4ffa6c8bce9137c84951b1b453016" "37768a79b479684b0756dec7c0fc7652082910c37d8863c35b702db3f16000f8" "197cefea731181f7be51e9d498b29fb44b51be33484b17416b9855a2c4243cb1" default))
  '(package-selected-packages
-   '(all-the-icons counsel fic-mode highlight-parentheses tl ace-window smartparens inf-ruby enh-ruby-mode nord-theme rbenv flycheck use-package)))
+   '(all-the-icons counsel fic-mode highlight-parentheses tl ace-window smartparens inf-ruby enh-ruby-mode nord-theme rbenv flycheck use-package))
+ '(safe-local-variable-values '((org-latex-caption-above))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
